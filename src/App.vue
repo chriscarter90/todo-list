6d3a336d0f9efc07a529a26e175dda76598a3181
Chris Carter <chris.carter@datacamp.com>
2019-05-03 10:49:57 +0100

    Use v-for and an array of lists

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <div class="ui three column doubling stackable grid container">
-      <todo-container :key="1" :todos="list1"></todo-container>
-      <todo-container :key="2" :todos="list2"></todo-container>
-      <todo-container :key="3" :todos="list3"></todo-container>
+      <todo-container v-for="(list, index) in lists" :key="index" :todos="list"></todo-container>
     </div>
   </div>
 </template>
@@ -18,9 +16,18 @@ export default {
   },
   data () {
     return {
-      list1: [{ title: 'a', project: 'a', done: false }],
-      list2: [{ title: 'd', project: 'd', done: false }, { title: 'b', project: 'b', done: false }],
-      list3: [{ title: 'c', project: 'c', done: false }]
+      lists: [
+        [
+          { title: 'a', project: 'a', done: false }
+        ],
+        [
+          { title: 'd', project: 'd', done: false },
+          { title: 'b', project: 'b', done: false }
+        ],
+        [
+          { title: 'c', project: 'c', done: false }
+        ]
+      ]
     }
   }
 }
